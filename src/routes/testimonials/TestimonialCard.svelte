f357661e68dfccb458263487a4ce5bd82e34a9cc
greduan <me@greduan.com>
2023-04-19 20:45:19 +0200

    Fix font-serif usage

--- src/routes/testimonials/TestimonialCard.svelte
+++ src/routes/testimonials/TestimonialCard.svelte
@@ -2,7 +2,7 @@
   export let title: string;
 </script>
 
-<div class="flex flex-col gap-8 mt-10 font-serif">
+<div class="flex flex-col gap-8 mt-10">
   <div class="flex flex-col p-8 bg-white rounded shadow">
     <div class="flex flex-col gap-4">
       <h3 class="font-bold text-cyan-800 text-lg">
