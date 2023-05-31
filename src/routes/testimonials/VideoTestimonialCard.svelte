ad0cf801f846f586f3ac197653a3c170c1ce6c26
greduan <me@greduan.com>
2023-05-31 20:39:00 +0200

    Center testimonial videos on mobile

--- src/routes/testimonials/VideoTestimonialCard.svelte
+++ src/routes/testimonials/VideoTestimonialCard.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div class="flex flex-col gap-8 mt-10">
-  <div class="flex flex-col md:flex-row-reverse gap-8 p-8 bg-white rounded shadow">
+  <div class="flex flex-col md:flex-row-reverse items-center md:items-start gap-8 p-8 bg-white rounded shadow">
     <div class="md:w-1/2 flex flex-col gap-4">
       <h3 class="font-bold text-cyan-800 text-lg">
         {title}
