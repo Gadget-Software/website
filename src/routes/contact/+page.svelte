ac2d1599f5a4c21ca23af074a3aba0132b14b780
Sanath Sambamoorthi <sanath001@gmail.com>
2025-09-12 13:46:32 +0900

    Create contact form

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+  let formStatus = "";
+</script>
+<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+
+<form 
+  action="https://api.web3forms.com/submit" 
+  method="POST" 
+  on:submit={() => formStatus = 'Submitting...'}
+>
+  <input type="hidden" name="access_key" value="7e754091-28ab-42ae-a916-8caa835f4549" />
+  <input type="hidden" name="subject" value="New Contact Message from Website" />
+
+  <label>
+    Name:
+    <input type="text" name="name" required />
+  </label>
+  <label>
+    Email:
+    <input type="email" name="email" required />
+  </label>
+  <label>
+    Message:
+    <textarea name="message" required></textarea>
+  </label>
+
+  <div 
+    class="cf-turnstile" 
+    data-sitekey="0x4AAAAAAB0wN7cDdLSLmIV5" 
+    data-theme="light"
+  ></div>
+
+  <button type="submit">Send</button>
+  <p>{formStatus}</p>
+</form>
