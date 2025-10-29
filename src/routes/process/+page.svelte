9e8e619c1c8064264eeaca5b59c35e1e5c3a1fa9
Sanath Sambamoorthi <sanath001@gmail.com>
2025-10-29 12:24:12 +0900

    Refine text content in +page.svelte
    Updated text content to enhance clarity and readability throughout the page.

--- src/routes/process/+page.svelte
+++ src/routes/process/+page.svelte
@@ -19,22 +19,22 @@
         <div class="w-full md:w-1/2">
           <p>
             In general, as an entrepreneur with limited financial and network resources, we might not be able to dominate the global market immediately. 
-<br />
+<br /><br />
             But we can dominate a small, local market.
-<br />
+<br /><br />
             As Sam Walton famously mentioned in his book on the legendary Walmart, Walmart was never able to compete with local businesses who knew their customers on a deeper level than Walmart was capable of allocating resources to. 
-<br />
+<br /><br />
             This is what makes the process of niching a necessary tool. The tighter we can narrow our customer profile, the easier we can identify common threads between customers, and the faster we can communicate with them in a compelling way, and the easier it is to deliver a solution that actually adds value to their lives in someway.
-<br />
+<br /><br />
             Secretly, there are other benefits as well, since a tight niche allows higher performing marketing, non-competitive pricing (meaning you can charge much higher prices), and easier targeting and access. 
           </p>
         </div>
         <figure class="w-full md:w-1/2 flex flex-col gap-4">
           <p>
             We work with the entrepreneur specifically to determine a niche that will easily accessible to them. 
-<br />
+<br /><br />
             Everyone who has worked for more than a few years has access to an existing set of potential customers they know really well. 
-<br />
+<br /><br />
             We hone in on where they the entrepreneur has a natural competitive advantage and determine the specifics of what this niche actually looks like using the existing information that's in their head.
           </p>
         </figure>
@@ -46,18 +46,18 @@
         <div class="w-full md:w-1/2">
           <p>
             Once a niche is selected, the next step is to know as much as possible about the customer profile.
-<br /> 
+<br /> <br />
             Things that initially appear insignificant could be discovered to be a gold mine of untapped potential later. 
-<br />      
+<br /> <br />   
             Once enough deep customer conversations are completed, a solid picture of the customer profile, their pains, their hopes, and more should start to become obvious to the entrepreneur. The marketing and sales communication is easy to build from this point, and also the value gaps for that person are clear, so the initial ideas for product should now come into shape.
           </p>
         </div>
         <figure class="w-full md:w-1/2 flex flex-col gap-4">
           <p>
             Once a targetted list of niche customers is identified, we must find out where they hang out, online, and in person. 
-<br />
+<br /> <br />
             At this point we knock on doors, email, DM, or whatever it takes to have conversations with them aiming for 30-50 discussions. 
-<br />
+<br /> <br />
             In this case, shadowing them for a day is superior to speaking to them which is superior to an online quiz. Even just 2-3 shadowing processes can reveal innumerable potential products. Whereas 50 could potentially result in none. 
           </p>
         </figure>
@@ -74,9 +74,9 @@
         <figure class="w-full md:w-1/2 flex flex-col gap-4">
           <p>
             We synthesize the data from the customer interviews and use the hypothesis from the interviewing process to be able to build, specifically a SaaS product that will hit with the target market. 
-            <br />
+            <br /><br />
             Since our experience is SaaS, we either build on behalf of the entrepreneur, or we manage the vendors who do. 
-            <br />
+            <br /><br />
             There is significant risk in this step, but planning to build something that can be tossed away or rebuilt later is intelligent at this stage of the business.
           </p>
         </figure>
@@ -88,18 +88,18 @@
         <div class="w-full md:w-1/2">
           <p>
             Once the product is built it's time to start onboarding users.  
-            <br />
+            <br /><br />
             This is where you harvest information related to user ROI, work out the kinks, errors, and other problems in the system, and start making it into a streamlined experience for your users.  
-            <br />
+            <br /><br />
             This will make sure that new users, especially those you don't know personally, have a good experience, leading to more efficient marketing pushes later in the user aquision step.
           </p>
         </div>
         <figure class="w-full md:w-1/2 flex flex-col gap-4">
           <p>
             The users you will onboard are specifically the people you interviewed at the start of this process. At this point it's pivotal to keep good ongoing dialogue with users, and watch them use the system ideally. 
-            <br />
+            <br /><br />
             Information from those conversations will inform ongoing development, and help make the system efficient to get into and easy to use.
-            <br />
+            <br /><br />
             We will also determine pricing structure at this step, incorporate payment, and do all the additional setup required for a revenue generating SaaS application.
           </p>
         </figure>
@@ -111,16 +111,16 @@
         <div class="w-full md:w-1/2">
           <p>
             Once the initial users have been onboarded, pricing has been determined, and we know we have product market fit.
-            <br />
+            <br /><br />
             This is where the entrepreneur's business becomes a real value generating and cash flow positive machine, and is the ultimate goal of this process.
           </p>
         </div>
         <figure class="w-full md:w-1/2 flex flex-col gap-4">
           <p>
             The next step of the process, and the final step of Gadget's involvement will be here. We will provide insight on scaling, how to market using Meta Ads, Email, Direct Mail, and other methods, and assist in distilling all the customer conversations into marketing pushes that work for the target market's psychology.
-            <br />
+            <br /><br />
             Additionally, we will do the preparation of the systems ourselves, or provide information for the software to handle the user counts that are being targetted by the entrepreneur. 
-            <br />
+            <br /><br />
             Our last step at this stage is to handoff the support that Gadget Software has provided to another party to ensure that the entrepreneur can continue without a hitch.
           </p>
         </figure>
