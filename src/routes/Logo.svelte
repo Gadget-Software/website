dbbc57c56a8f5ca4c3a99a36ab439628dcce05dc
Sanath Sambamoorthi <sanath001@gmail.com>
2025-12-12 16:21:47 +0900

    Update logo text to reflect new branding

--- src/routes/Logo.svelte
+++ src/routes/Logo.svelte
@@ -1,4 +1,4 @@
 <a class="flex items-center gap-2" href="/">
   <img alt="Gadget Software logo" src="/logo-elephant.png" class="h-8" />
-  <div class="text-lg text-gray-600">Gadget Software + Marketing</div>
+  <div class="text-lg text-gray-600">Sanath Sambamoorthi Software + Marketing</div>
 </a>
