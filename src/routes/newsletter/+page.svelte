6871437b0fe84e4bcddfd6639edf4ec63d52466d
Sanath Sambamoorthi <sanath001@gmail.com>
2026-01-20 20:53:24 +0900

    Update newsletter page for Fundamentals Course

--- src/routes/newsletter/+page.svelte
+++ src/routes/newsletter/+page.svelte
@@ -2,10 +2,10 @@
 </script>
 
 <svelte:head>
-  <title>Direct Email Access | Sanath Sambamoorthi</title>
+  <title>Fundamentals Course | Sanath Sambamoorthi</title>
   <meta
     name="description"
-    content="Get direct email access to Sanath. Real-time insights from building and scaling personal brands, plus products and recommendations I'm actually using."
+    content="New Client Fundamentals Course"
   />
 </svelte:head>
 
@@ -14,10 +14,14 @@
   <div class="max-w-screen-lg mx-auto px-4">
     <div class="text-center py-14 md:py-24 flex flex-col gap-4 md:gap-6">
       <h1 class="text-3xl md:text-5xl font-semibold leading-tight">
-        Get <span class="text-cyan-300"> direct email access</span>
+        Get <span class="text-cyan-300"> New Client Fundamentals Course</span>
       </h1>
       <p class="text-lg md:text-2xl text-cyan-100">
-        This is not “another newsletter.” This is direct contact with me.
+        I am currently booked out, but if are still interested in working with me, this is an incredible primer on my methodology of working.
+      </p>
+      </br>
+      <p class="text-lg md:text-2xl text-cyan-100">
+        Delivered over 4 weeks, this is the battle tested output of the last decade of knowledge of taking self development course after self development course. and throwing myself into the fire in every domain of life.
       </p>
     </div>
   </div>
@@ -27,17 +31,18 @@
 <section class="bg-white text-cyan-900">
   <div class="max-w-screen-lg mx-auto px-4">
     <div class="py-10 md:py-16 grid gap-8 text-center">
-      <h2 class="text-2xl md:text-3xl font-semibold">        
-        I’ll occasionally send you emails with what I’m building, what I’m testing with
-        clients, and what’s actually working for people who are starting, and scaling
-        their personal brands.
+      <h2 class="text-2xl md:text-3xl font-semibold">
+        After the course is over, I’ll occasionally send you emails with what I’m building, what I’m testing with
+        clients, and what’s actually working for people who are starting, and scaling their personal brands. 
+
+        Additionally, if client spots open up in the future, this is the list that will be the first to know.
       </h2>
       <script async
         src="https://js.stripe.com/v3/buy-button.js">
       </script>
       
       <stripe-buy-button
-        buy-button-id="buy_btn_1Sp43YGz1PAG6fEyUWNjx5P5"
+        buy-button-id="buy_btn_1SrdTiGz1PAG6fEynOzk5GBX"
         publishable-key="pk_live_51SbVFiGz1PAG6fEytPo6YrjWnxYKizd3jrQY7DOogdEzsYxwE9wp69HhhqMJNSBIULH0YPMdoNa0ysrNKEjS4gij000WGx1kna"
       >
       </stripe-buy-button>
